f351f8f5021881bca418090baf9b85a864bf4021
KatOlista <olya.katyushina@gmail.com>
2021-05-11 12:20:19 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,81 @@
+let money = +prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
+};
+  
+
+// for (let i = 0; i < 2; i++) {
+// 	let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+// 		b = prompt("Во сколько обойдется?", '');
+
+// 	if( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null
+// 	&& a != '' && b != '' && a.length < 50) {
+// 		console.log(a, b);
+// 		appData.expenses[a] = b;
+		
+// 	} else {
+// 		console.log("Vy vveli nekorrektnyje dannyje, poprobujte jesze raz");
+// 		i--;
+		
+// 	}
+// };
+
+// let i = 0; 
+// 	do {
+// let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+// 	b = prompt("Во сколько обойдется?", '');
+	
+// 		if( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null
+// 		&& a != '' && b != '' && a.length < 50) {
+// 			console.log(a, b);
+// 			appData.expenses[a] = b;
+			
+// 		} else {
+// 			console.log("Vy vveli nekorrektnyje dannyje, poprobujte jesze raz");
+// 			i--;
+// 		}
+// 		i++;
+// } while (i < 2) ;
+
+let i = 0;
+while (i < 2) {
+	let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	b = prompt("Во сколько обойдется?", '');
+	if( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null
+		&& a != '' && b != '' && a.length < 50) {
+			console.log(a, b);
+			appData.expenses[a] = b;
+			
+		} else {
+			console.log("Vy vveli nekorrektnyje dannyje, poprobujte jesze raz");
+			i--;
+		}
+		i++;
+}
+
+
+
+
+
+
+
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Budzet na dien = " + appData.moneyPerDay);
+if(appData.moneyPerDay < 100) {
+	console.log("Minimalnyj uroven dostatka")
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+console.log("Srednij uroven dostatka");
+} else if(appData.moneyPerDay > 2000) {
+	console.log("Vysokij uroven dostatka");
+} else {
+	console.log("Proizoszla oszybka");
+};
